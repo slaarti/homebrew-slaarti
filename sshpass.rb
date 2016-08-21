8ab8b6944714320b5bbcdfd3ff5e1669b671a36a
Chris Pinard <slarti@gweep.net>
2016-08-21 10:10:33 -0400

    Update to sshpass 1.06, switch from md5 to sha256.

--- sshpass.rb
+++ sshpass.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Sshpass < Formula
-  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.05/sshpass-1.05.tar.gz'
+  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.06/sshpass-1.06.tar.gz'
   homepage 'http://sourceforge.net/projects/sshpass'
-  md5 'c52d65fdee0712af6f77eb2b60974ac7'
+  sha256 'c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
